bbf93447d28c3be06062767ccf8d1fc642487294
Mohammed Saqib <113831139+saqib777@users.noreply.github.com>
2025-12-26 15:59:46 +0530

    Update test_login.py

--- tests/test_login.py
+++ tests/test_login.py
@@ -1,3 +1,18 @@
-def test_valid_login():
-    pass
+from pages.login_page import LoginPage
 
+
+def test_valid_login(driver):
+    """
+    Verify that a user can log in with valid credentials.
+    """
+    login_page = LoginPage(driver)
+
+    login_page.open()
+    login_page.login(
+        username="tomsmith",
+        password="SuperSecretPassword!"
+    )
+
+    success_message = login_page.get_success_message()
+
+    assert "You logged into a secure area!" in success_message

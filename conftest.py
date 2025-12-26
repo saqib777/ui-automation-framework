f45bc20058a848e923167d2a6cb30c6f855cd12e
Mohammed Saqib <113831139+saqib777@users.noreply.github.com>
2025-12-26 15:47:30 +0530

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -1,1 +1,14 @@
+import pytest
+from drivers.driver_factory import create_driver, quit_driver
+
+
+@pytest.fixture
+def driver():
+    """
+    Pytest fixture to provide a WebDriver instance to tests.
+    Handles setup and teardown automatically.
+    """
+    driver = create_driver()
+    yield driver
+    quit_driver(driver)
 

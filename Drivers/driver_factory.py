1cae4467ed144b3d5924fed80b6c6ac7b7608d44
Mohammed Saqib <113831139+saqib777@users.noreply.github.com>
2025-12-26 15:43:13 +0530

    Update driver_factory.py

--- Drivers/driver_factory.py
+++ Drivers/driver_factory.py
@@ -1,1 +1,24 @@
 
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+
+
+def create_driver():
+    """
+    Creates and returns a configured Chrome WebDriver instance.
+    """
+    chrome_options = Options()
+    chrome_options.add_argument("--start-maximized")
+
+    driver = webdriver.Chrome(options=chrome_options)
+    return driver
+
+
+def quit_driver(driver):
+    """
+    Safely quits the WebDriver instance.
+    """
+    if driver:
+        driver.quit()
+
